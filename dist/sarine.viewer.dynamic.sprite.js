1317377c4cab589c20a5846a8e0bf311628f560a
SarineColor <github@sarine.com>
2016-06-26 17:11:38 +0300

    deploy 0.5.0

--- dist/sarine.viewer.dynamic.sprite.js
+++ dist/sarine.viewer.dynamic.sprite.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.dynamic.sprite - v0.4.0 -  Sunday, June 26th, 2016, 5:04:55 PM 
+sarine.viewer.dynamic.sprite - v0.5.0 -  Sunday, June 26th, 2016, 5:11:33 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
