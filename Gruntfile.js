9ef847e332742b6c56a68618664882600ca6ef46
ronishoham <roni.shoham@sarine.com>
2018-02-07 14:49:00 +0200

    change sarine.viewer path

--- Gruntfile.js
+++ Gruntfile.js
@@ -149,7 +149,7 @@ module.exports = function(grunt) {
         {
             core = 
             [
-                '../../core/sarine.viewer/coffee/sarine.viewer.coffee',
+                '../sarine.viewer/coffee/sarine.viewer.coffee',
                 '../sarine.viewer.dynamic/coffee/sarine.viewer.dynamic.coffee'
             ]
 
